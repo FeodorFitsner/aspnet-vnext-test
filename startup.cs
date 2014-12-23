dafd8e9bcaae7fc83e22d4d324cc66c19c24ca7e
Feodor Fitsner <feodor@appveyor.com>
2014-12-23 10:14:24 -0800

    Create startup.cs

---
+++ startup.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+ 
+public class Startup
+{
+    public void Configure(IApplicationBuilder app)
+    {
+        app.Use((context, next) =>
+        {
+           context.Response.ContentType = "text/plain";
+           context.Response.WriteAsync("Hello ");
+ 
+           return next.Invoke();
+        });
+ 
+        app.Run((HttpContext context) =>
+        {
+           return context.Response.WriteAsync("ASP.NET vNext");
+        });
+}
